3ed597dcabb90afe8a29f49a535015e02ba7608d
Rubí <rubi_salcedocas@outlook.es>
2023-12-20 21:12:20 -0700

    New

---
+++ src/SentenceSwitch.java
@@ -0,0 +1,25 @@
+public class SentenceSwitch {
+    public static void main (String[] args){
+
+        String colorModeSelected = "Dark";
+
+        switch (colorModeSelected) {
+            case "Light":
+                System.out.println("Selecionaste Light Mode");
+                break;
+            case "Night":
+                System.out.println("Selecionaste Night Mode");
+                break;
+            case "Blue Dark":
+                System.out.println("Selecionaste Blue Dark Mode");
+                break;
+            case "Dark":
+                System.out.println("Selecionaste Dark Mode");
+                break;
+        
+            default:
+                System.out.println("Seleciona una opcion");
+                break;
+        }
+    }
+}
